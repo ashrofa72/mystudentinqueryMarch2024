590421ca59daa78edcf6ff8204593bbea3338aa9
Ashraf <as.ka1@hotmail.com>
2024-03-31 20:53:50 +0200

    new  commit

---
+++ components/Navbar.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Link from 'next/link'
+import styles from '../styles/Nav.module.css'
+
+const Navbar = () => {
+    return (
+        <div  className={styles.container}>
+            <nav className={styles.nav}>
+                <ul>
+                    <li>
+                        <Link href='/'>Home</Link>
+                    </li>
+                    <li>
+                        <Link href='/users'>Users</Link>
+                    </li>
+                    <li>
+                        <Link href='/addinfo'>AddPost</Link>
+                    </li>
+                    <li>
+                        <Link href='/replay/'>Posts</Link>
+                    </li>
+                    <li>
+                        <Link href='/SearchByname/'>Search</Link>
+                    </li>
+                    <li>
+                        <Link href='/bookstore/'>Books</Link>
+                    </li>
+                    <li>
+                        <Link href='/goldprices/'>Gold</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar
